c594375587252c7b2a9828221f84846739b446ac
AffreuxJojo11 <jordan.ben-mihoub@proton.me>
2024-05-07 04:02:08 +0000

    RDP-Reading-Data-with-Python-and-Pandas

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,15 @@
 import numpy as np
 
-def calculate(list):
-
-
-
-
+def calculate(myList):
+    if len(myList) == 9:
+        myArray = np.array([myList[:3],myList[3:-3],myList[-3:]])
+        calculations                       = {}
+        calculations['mean']               = [myArray.mean(axis=0).tolist(), myArray.mean(axis=1).tolist(), myArray.mean()]
+        calculations['variance']           = [myArray.var(axis=0).tolist(), myArray.var(axis=1).tolist(), myArray.var()]
+        calculations['standard deviation'] = [myArray.std(axis=0).tolist(), myArray.std(axis=1).tolist(), myArray.std()]
+        calculations['max']                = [myArray.max(axis=0).tolist(), myArray.max(axis=1).tolist(), myArray.max()]
+        calculations['min']                = [myArray.min(axis=0).tolist(), myArray.min(axis=1).tolist(), myArray.min()]
+        calculations['sum']                = [myArray.sum(axis=0).tolist(), myArray.sum(axis=1).tolist(), myArray.sum()]
+    else:
+        raise ValueError('List must contain nine numbers.')
     return calculations
